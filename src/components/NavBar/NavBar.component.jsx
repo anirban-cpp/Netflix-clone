2968c5f7b3bccbf7d0aef68e56e1d26461639b71
Anirban Banerjee <Anirban Banerjee>
2021-03-19 19:00:19 +0530

    header changed

--- src/components/NavBar/NavBar.component.jsx
+++ src/components/NavBar/NavBar.component.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
 import './NavBar.styles.css'
+import { IconButton } from '@material-ui/core';
 
 function NavBar() {
 
@@ -19,16 +23,40 @@ function NavBar() {
         <div className='navBar'>
             <div className={`nav ${show && 'nav_black'}`}>
                 <div className="nav__contents">
-                    <img
-                        className='nav_logo'
-                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
-                        alt=""
-                    />
-                    <img
-                        className='nav_avatar'
-                        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                        alt=""
-                    />
+                    <div  className="nav__left__contents">
+                        <div>
+                            <img
+                                className='nav_logo'
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+                                alt=""
+                            />
+                        </div>
+                        <div className="nav__contents__options" style={{paddingLeft: '120px'}}>
+                            <div className='nav_option' ><h5>Home</h5></div>
+                            <div className='nav_option' ><h5>Series</h5></div>
+                            <div className='nav_option' ><h5>Movies</h5></div>
+                            <div className='nav_option' ><h5>Popular News</h5></div>
+                            <div className='nav_option' ><h5>My List</h5></div>
+                        </div>
+                    </div>
+                    <div/>
+                    <div className="nav__right__contents">
+                        <div className='nav__contents__options' style={{paddingRight: '80px'}}>
+                            <IconButton style={{padding: '0', minHeight: '0', minWidth: '0'}}>
+                                <SearchIcon style={{color: 'white', marginRight: '10px'}}/>
+                            </IconButton>
+                            <IconButton style={{padding: '0', minHeight: '0', minWidth: '0'}}>
+                                <NotificationsIcon style={{color: 'white'}}/>
+                            </IconButton>
+                        </div>
+                        <div>
+                            <img
+                            className='nav_avatar'
+                            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                            alt=""
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
